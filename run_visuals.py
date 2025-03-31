32fd3f148e90ba31be0e9bc6015a88473fb829c7
nanoludvig <106431057+nanoludvig@users.noreply.github.com>
2025-03-31 15:06:50 +0200

    Add files via upload

---
+++ run_visuals.py
@@ -0,0 +1,11 @@
+from animate_function import *
+
+
+
+
+file_path = 'test_output/sim_test1.npy'
+interactive_animate(file_path, polar='pcp', polarity_style='balls', view_style='fly')
+
+# Polar = 'AB' or 'pcp'
+# Polarity_style = 'balls' or 'rods'
+# View_style = 'arcball' or 'fly'
